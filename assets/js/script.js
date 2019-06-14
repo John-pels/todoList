ac8c065a6471ac88accd49a37823b54565cf45e2
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-14 13:13:13 +0100

    Finished the Js interactiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
 //check off specific todos by clicking
-$("li").click(function() {
-    //if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        //turn it black
-        // turn it gray
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+
+});
+//click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+$("input[type ='text']").keypress(function() {
+    if (event.which === 13) {
+        //grabbing new todo text from input box
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
     }
-
-    // else
-    else {
-        // turn it gray
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
-
-
 });
