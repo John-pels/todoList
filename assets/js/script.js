3bf1537135b6e16724647afc208bec68a55e0d56
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-14 12:26:47 +0100

    Adding Js script to the page

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//check off specific todos by clicking
+$("li").click(function() {
+    //if li is gray
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        //turn it black
+        // turn it gray
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    }
+
+    // else
+    else {
+        // turn it gray
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+
+
+});
